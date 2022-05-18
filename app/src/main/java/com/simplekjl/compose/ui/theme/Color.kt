f25247e46d1c8c6a7a9269e772bc5792eb974272
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-05-18 12:32:15 +0200

    updated colors and fonts

--- app/src/main/java/com/simplekjl/compose/ui/theme/Color.kt
+++ app/src/main/java/com/simplekjl/compose/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.simplekjl.compose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Green1 = Color(0xFF45D368)
+val Green2 = Color(0xFF4FAF7E)
+val Green3 = Color(0xFF57AD59)
 val Teal200 = Color(0xFF03DAC5)
--- app/src/main/java/com/simplekjl/compose/ui/theme/Type.kt
+++ app/src/main/java/com/simplekjl/compose/ui/theme/Type.kt
@@ -2,16 +2,28 @@ package com.simplekjl.compose.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.simplekjl.compose.R
+
+// Initializing the new font
+
+val robotoFamily = FontFamily(
+    Font(R.font.roboto_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = robotoFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Normal
     )
     /* Other default text styles to override
     button = TextStyle(
