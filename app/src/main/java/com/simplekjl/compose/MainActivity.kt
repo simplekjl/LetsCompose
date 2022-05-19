752168e765c636799919df1352f3c19e37c4ddaa
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-05-19 12:47:30 +0200

    Added color based on the state of the card

--- app/src/main/java/com/simplekjl/compose/MainActivity.kt
+++ app/src/main/java/com/simplekjl/compose/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,6 +23,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -64,19 +71,36 @@ fun MessageCard(msg: Message) {
             error = painterResource(id = R.drawable.ghost)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        // state of the card, use to show max lines
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            targetValue = if (isExpanded)
+                MaterialTheme.colors.primary
+            else
+                MaterialTheme.colors.surface
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
-
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.body2
+                    style = MaterialTheme.typography.body2,
+                    // if expanded then show the entire message
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
                 )
             }
         }
